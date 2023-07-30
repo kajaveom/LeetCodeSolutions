dd8f226d057a1e2f3eca7bb2636ee3e30453dc0f
OM ANIL KAJAVE <89060761+kajaveom@users.noreply.github.com>
2023-07-30 11:03:51 +0530

    Added solution - LeetHub

--- Wildcard string matching - GFG/wildcard-string-matching.cpp
+++ Wildcard string matching - GFG/wildcard-string-matching.cpp
@@ -42,11 +42,57 @@ class Solution{
         return dp[i][j] = false;
     }
     
-    bool match(string wild, string pattern)
+    bool match(string w, string p)
     {
-        int n = wild.size(), m = pattern.size();
-        vector<vector<int>> dp(n, vector<int>(m,-1));
-        return canMatch(wild, pattern, 0, 0, dp);
+        int n = w.size(), m = p.size();
+        vector<vector<bool>> dp(n+1, vector<bool>(m+1,false));
+        
+        dp[n][m] = true;
+        for(int i = n-1; i >= 0; i--){
+            if(w[i]=='*')
+            dp[i][m] = dp[i+1][m];
+            else
+            dp[i][m] = false;
+        }
+        
+        for(int j = m-1; j >= 0; j--){
+            dp[n][j] = false;
+        }
+        
+        
+        for(int i = n-1; i >= 0 ; i--){
+            for(int j = m-1; j >= 0; j--){
+                
+                if(w[i]==p[j]){
+                    dp[i][j] = dp[i+1][j+1];
+                }
+                else if(w[i] == '*'){
+                    bool op1 = dp[i][j+1];
+                    bool op2 = dp[i+1][j];
+                    
+                    //cout<<i<<" "<<j<<" "<<op1<<" "<<op2<<endl;
+                    
+                    dp[i][j] = (op1 | op2);
+                }
+                else if(w[i] == '?'){
+                    dp[i][j] = dp[i+1][j+1];
+                }
+                else
+                dp[i][j] = 0;
+                
+            }
+        }
+        
+        
+        // for(int i = 0; i <= n; i++){
+        //     for(int j = 0; j <= m; j++)
+        //     cout<<dp[i][j]<<" ";
+        //     cout<<endl;
+        // }
+        
+        
+        
+        return  dp[0][0];
     }
 };
 

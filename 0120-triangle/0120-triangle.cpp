1010dee7f032e6a002597fd717798041127af7c9
OM ANIL KAJAVE <89060761+kajaveom@users.noreply.github.com>
2023-08-03 22:40:57 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (79.19%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
     
-    int getMinPath(vector<vector<int>> &t, int i, int j, vector<vector<int>> &dp){
+//     int getMinPath(vector<vector<int>> &t, int i, int j, vector<vector<int>> &dp){
         
-        if(i == t.size()-1)
-            return t[i][j];
+//         if(i == t.size()-1)
+//             return t[i][j];
         
-        if(dp[i][j]!=-1)
-            return dp[i][j];
+//         if(dp[i][j]!=-1)
+//             return dp[i][j];
         
-        return dp[i][j] = t[i][j]  + min( getMinPath(t,i+1,j, dp)  ,  getMinPath(t,i+1,j+1, dp) );
-    }
+//         return dp[i][j] = t[i][j]  + min( getMinPath(t,i+1,j, dp)  ,  getMinPath(t,i+1,j+1, dp) );
+//     }
     
     int minimumTotal(vector<vector<int>>& triangle) {
         
         int n = triangle.size();
-        vector<vector<int>> dp(n, vector<int>(n ,-1));
+        vector<int> dp(n,0);
         
         for(int j = 0; j < n; j++){
-            dp[n-1][j] = triangle[n-1][j];
+            dp[j] = triangle[n-1][j];
         }
         
         for(int i=n-2; i >= 0; i--){
-            for(int j = i; j >= 0; j--)
-            dp[i][j] = triangle[i][j]  + min( dp[i+1][j] , dp[i+1][j+1] );
+            for(int j = 0; j <= i; j++)
+            dp[j] = triangle[i][j]  + min( dp[j] , dp[j+1] );
             
         }
         
-        return dp[0][0];
+        return dp[0];
         
     }
 };

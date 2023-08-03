a3cbce4fa7565b332e0856a25adcf88bc18a1557
OM ANIL KAJAVE <89060761+kajaveom@users.noreply.github.com>
2023-08-03 22:36:26 +0530

    Time: 4 ms (87.02%), Space: 9.1 MB (18.81%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -14,9 +14,20 @@ public:
     
     int minimumTotal(vector<vector<int>>& triangle) {
         
-        int n = triangle.size(), m = triangle.size();
-        vector<vector<int>> dp(n, vector<int>(m ,-1));
-        return getMinPath(triangle,0,0,dp);
+        int n = triangle.size();
+        vector<vector<int>> dp(n, vector<int>(n ,-1));
+        
+        for(int j = 0; j < n; j++){
+            dp[n-1][j] = triangle[n-1][j];
+        }
+        
+        for(int i=n-2; i >= 0; i--){
+            for(int j = i; j >= 0; j--)
+            dp[i][j] = triangle[i][j]  + min( dp[i+1][j] , dp[i+1][j+1] );
+            
+        }
+        
+        return dp[0][0];
         
     }
 };

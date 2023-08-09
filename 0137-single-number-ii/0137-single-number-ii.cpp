5d1399e47e4331ed9e5eeb2fbcab700fdeaaa169
OM ANIL KAJAVE <89060761+kajaveom@users.noreply.github.com>
2023-08-09 19:54:20 +0530

    Time: 7 ms (81.51%), Space: 9.5 MB (65.83%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        
+        int ones= 0, twos =0;
+        for(auto n : nums){
+            ones = (ones^n) & (~twos);
+            twos = (twos^n) & (~ones);
+        }
+        
+        return ones;
+    }
+};

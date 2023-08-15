7841b7717fb279f7286295e3d97c56a466af270f
OM ANIL KAJAVE <89060761+kajaveom@users.noreply.github.com>
2023-08-15 21:56:38 +0530

    Time: 45 ms (19.44%), Space: 18.5 MB (40.23%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,31 +1,47 @@
 class Solution {
 public:
     
-    int totWays(vector<int> &c, int i, int a, vector<vector<int>> &dp){
+//     int totWays(vector<int> &c, int i, int a, vector<vector<int>> &dp){
         
-        if(a == 0)
-            return 1;
+//         if(a == 0)
+//             return 1;
         
-        if(a < 0)
-            return 0;
+//         if(a < 0)
+//             return 0;
         
-        if(i >= c.size())
-            return 0;
+//         if(i >= c.size())
+//             return 0;
         
-        if(dp[i][a]!=-1)
-            return dp[i][a];
+//         if(dp[i][a]!=-1)
+//             return dp[i][a];
         
-        int w1 = totWays(c, i+1, a, dp);
-        int w2 = totWays(c, i, a - c[i], dp);
+//         int w1 = totWays(c, i+1, a, dp);
+//         int w2 = totWays(c, i, a - c[i], dp);
         
-        return dp[i][a] = w1 + w2;
-    }
+//         return dp[i][a] = w1 + w2;
+//     }
     
     int change(int amount, vector<int>& coins) {
         
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount+1, -1));
+        vector<vector<int>> dp(n+1, vector<int>(amount+1, 0));
         
-        return totWays(coins,0,amount, dp);
+        for(int i = 0; i <= amount; i++)
+            dp[n][i] = 0;
+        for(int i = 0 ; i <= n; i++){
+            dp[i][0] = 1;
+        }
+        
+        for(int i = n-1; i >= 0; i--){
+            for(int a = 0; a <= amount ; a++){
+                
+                dp[i][a] = dp[i+1][a];
+                if(a - coins[i] >= 0)
+                    dp[i][a] += dp[i][a - coins[i]];
+                
+            }
+        }
+        
+        return dp[0][amount];
     }
 };
